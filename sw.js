518868a00b2c04a5775eff87db2bcabe597b6aa9
Sam <samuel@dev-null.li>
2025-07-24 14:11:00 +0200

    avoid outdated sw cache

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
+const CACHE_NAME = 'sspg-v1.2';
+const CACHE_FILES = [
+  '/index.html',
+  '/css/style.css',
+  '/js/sspg.js',
+];
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('sams-secure-password-generator').then((cache) => {
-      cache.addAll([
-        '/index.html',
-        '/css/style.css',
-        '/js/sspg.js'
-      ])
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
     })
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
-})
+});
