1cf8814c6aec33100d08524d6239b567a84cfc02
Sam <samuel@dev-null.li>
2026-06-09 17:26:04 +0200

    improve and version .htaccess

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'sspg-v1.3.1';
+const CACHE_NAME = 'sspg-v1.3.2';
 const CACHE_FILES = [
   '/',
   '/index.html',
-  '/css/style.css?v=1.3.1',
-  '/js/sspg.js?v=1.3.1',
-  '/register-sw.js?v=1.3.1',
+  '/css/style.css?v=1.3.2',
+  '/js/sspg.js?v=1.3.2',
+  '/register-sw.js?v=1.3.2',
   '/assets/pwa/manifest.json',
   '/assets/favicon/android-icon-192x192.png',
   '/assets/favicon/android-icon-512x512.png',
